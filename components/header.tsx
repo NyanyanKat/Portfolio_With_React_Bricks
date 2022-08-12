7090b30d5393e942713e7b015ceeaaf7be5a1684
Jimmy Chan <41240749+NyanyanKat@users.noreply.github.com>
2022-08-11 21:51:12 -0700

    Updated portfolio

--- components/header.tsx
+++ components/header.tsx
@@ -10,21 +10,27 @@ const Header: React.FC = () => (
           <h1 className="text-white">Jimmy's Portfolio</h1>
           <div className="sm:ml-8 flex space-x-5 text-center">
             <Link href="/">
-              <a className="text-white hover:text-pink-700">Home</a>
-            </Link>
-            <Link href="/about-us">
               <a className="text-white hover:text-pink-700">About me</a>
             </Link>
-            <Link href="/blog">
+            <Link href="/skills">
+              <a className="text-white hover:text-pink-700">Skills</a>
+            </Link>
+            <Link href="/projects">
               <a className="text-white hover:text-pink-700">Projects</a>
+            </Link>
+            <Link href="/experience">
+              <a className="text-white hover:text-pink-700">Experience</a>
+            </Link>
+            <Link href="/education">
+              <a className="text-white hover:text-pink-700">Education</a>
             </Link>
           </div>
         </div>
-        <Link href="/admin" prefetch={false}>
+        {/* <Link href="/admin" prefetch={false}>
           <a className="py-2 px-5 rounded text-white font-medium bg-cyan-500 hover:bg-cyan-600 hover:shadow-lg transition duration-200">
             Edit content
           </a>
-        </Link>
+        </Link> */}
       </div>
     </div>
   </header>
